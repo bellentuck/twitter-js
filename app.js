91c2e9bf65851e7f7b548fc8dddc266a997c2cc2
Edison Alulema <edisonalulema@Edisons-MacBook-Air.local>
2018-03-06 12:23:28 -0500

    styling

--- app.js
+++ app.js
@@ -3,26 +3,17 @@ const express = require('express');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const app = express();
+const routes = require('./routes');
+
 
 // MORGAN CONFIG ------------------ //
 app.use(morgan('dev'));
-/// vs.:
-// app.use(function(req, res, next){
-//   console.log(req.method, res.statusCode, req.url);
-//   next()
-// });
+app.use(express.static('public'))
+
 
 // ROUTES ------&----------- //
 // RENDERING ------------------ //
-app.get('/', function(req, res, next) {
-  //res.send('Welcome!');
-  res.render('index', {title: 'hall of fame', people: people});
-  // nunjucks.render('index.html', people, function(err, template) {
-  //   if (err) throw err;
-  //   //console.log(data);
-  //   res.send(template);
-  // });
-})
+app.use('/', routes);
 
 // LISTEN STATEMT ------------------ //
 app.listen(3000, function() {
@@ -56,4 +47,5 @@ app.engine('html', nunjucks.render);
 nunjucks.configure('views', {
   noCache: true
 });
+
 
